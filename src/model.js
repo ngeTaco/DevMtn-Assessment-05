003c40521ea718a8f3521510110d9c896919a38c
ngeTaco <thealdo.531@gmail.com>
2024-07-12 17:47:03 -0600

    animals models added

--- src/model.js
+++ src/model.js
@@ -14,7 +14,31 @@ export class Human extends Model {
   }
 }
 
-// TODO: Human.init()
+Human.init(
+  {
+    humanId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    fname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    modelName: 'humans',
+    sequelize: db,
+  },
+);
 
 export class Animal extends Model {
   [util.inspect.custom]() {
@@ -22,7 +46,30 @@ export class Animal extends Model {
   }
 }
 
-// TODO: Animal.init()
+Animal.init(
+  {
+    animalId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    species: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    birthYear: {
+      type: DataTypes.INTEGER,
+    },
+  },
+  {
+    modelName: 'animals',
+    sequelize: db,
+  },
+);
 
 // TODO: Define Relationship
 
