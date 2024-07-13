04f2f92c822b3a020308208edd64164aaad3c064
ngeTaco <thealdo.531@gmail.com>
2024-07-12 20:12:44 -0600

    animals query 1 through 8 complete

--- src/queries.js
+++ src/queries.js
@@ -2,28 +2,29 @@ import { Op } from 'sequelize';
 import { Animal, Human } from './model.js';
 
 // Get the human with the primary key 2
-export const query1 = null;
+export const query1 = await Human.findByPk(2);
 
 // Get the first animal whose species is "fish"
-export const query2 = null;
+export const query2 = await Animal.findOne({ where: { species: "fish" } });
 
 // Get all animals belonging to the human with primary key 5
-export const query3 = null;
+export const query3 = await Animal.findAll({ where: { humanId: 5} });
 
 // Get all animals born in a year greater than (but not equal to) 2015.
-export const query4 = null;
+export const query4 = await Animal.findAll({ where: { birthYear: { [Op.gt]: 2015}} });
 
 // Get all the humans with first names that start with "J"
-export const query5 = null;
+export const query5 = await Human.findAll({ where: { fname: {[Op.startsWith]: "J"} } });
 
 // Get all the animals who don't have a birth year
-export const query6 = null;
+export const query6 = await Animal.findAll({ where: { birthYear: null} });
 
 // Get all the animals with species "fish" OR "rabbit"
-export const query7 = null;
+export const query7 = await Animal.findAll({ where: { [Op.or]: [{species: "fish"}, {species: "rabbit"}] } });
 
 // Get all the humans who DON'T have an email address that contains "gmail"
-export const query8 = null;
+export const query8 = await Human.findAll({ where: { email: { [Op.notLike]: '%gmail%' } } });
+//Human.findAll({ where: { [Op.not]: { email: { [Op.contains]: ["gmail"] } } } });
 
 // Continue reading the instructions before you move on!
 
